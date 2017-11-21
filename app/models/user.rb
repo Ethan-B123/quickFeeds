90766eeb2c282f5a88c96e76ee2b609be837823e
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-11-21 13:48:54 -0800

    create user model and auth routes

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    end
+    nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    save
+  end
+
+  def password=(new_pass)
+    @password = new_pass
+    self.password_digest = BCrypt::Password.create(new_pass)
+  end
+
+  def is_password?(check_pass)
+    BCrypt::Password.new(password_digest).is_password?(check_pass)
+  end
+
+end
