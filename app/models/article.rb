55e1c1165699f1e317ed00717e65f7abfab6136a
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-11-24 09:59:23 -0800

    finish feed and article models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,27 +1,44 @@
 class Article < ApplicationRecord
-  validates :feed_id, :title, :image, :description, :publish_date, presence: true
+  validates :title, :image, :description, :publish_date, presence: true
   validates :url, uniqueness: true
 
+  belongs_to :feed
+
+  def self.sort_and_delete(feed_articles, limit = 100)
+    # sorts feed_articles by age & keeps the youngest 100
+    # TODO: don't delete if in saves table
+    if feed_articles.length > limit
+      article_arr = feed_articles.to_a
+      article_arr.sort! do |article1, article2|
+        article2.publish_date <=> article1.publish_date
+      end
+      # single hit on db
+      Article.where(feed_id: feed_articles.first.feed_id)
+        .where("publish_date < ?", article_arr[limit].publish_date)
+        .delete_all
+    end
+  end
+
   def populate_attributes(rss_entry)
-    props = %w(url title image description publish_date)
     find_alias(:title, rss_entry, "title")
     find_alias(:url, rss_entry, "url")
     find_alias(:description, rss_entry, "description", "summary")
     find_alias(:image, rss_entry, "image", "image_url", "enclosure_url")
     find_alias(:publish_date, rss_entry, "publish_date", "published")
     self.description = description.html_safe
+    self
   end
 
   private
 
-  def find_alias(prop, item, *aliases)
+  def find_alias(prop, rss_item, *aliases)
     aliases.each do |ali|
-      if item.respond_to? ali
-        self.send((prop.to_s + "="), item.send(ali))
-        return item.send(ali)
+      if rss_item.respond_to?(ali) && !rss_item.send(ali).nil?
+        self.send((prop.to_s + "="), rss_item.send(ali))
+        return rss_item.send(ali)
       end
     end
-    self.send((prop.to_s + "="), item)
+    self.send((prop.to_s + "="), "No #{prop} data available")
     false
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,14 +2,30 @@ class Feed < ApplicationRecord
   validates :description, :title, presence: true
   validates :url, presence: true, uniqueness: true
   attr_accessor :response
-  attr_accessor :articles
 
-  def go(url) # 4 TESTING
-    self.url = url
-    if fetch_data
-      populate_attributes
-      build_articles
+  has_many :articles
+
+  # on new: if url in database
+    # return that one
+  # else
+    # if fetch_data (aka vaild rss url)
+      # save it
+    # else
+      # return nil / throw error
+    # end
+  # end
+
+  def self.make_with_url(url)
+    if exists = Feed.find_by(url: url)
+      return exists
+    else
+      newFeed = Feed.new({ url: url })
+      if newFeed.fetch_data
+        # newFeed.save
+        return newFeed
+      end
     end
+    false
   end
 
   def fetch_data(&prc)
@@ -18,43 +34,77 @@ class Feed < ApplicationRecord
       xml = HTTParty.get(url).body
       feed = Feedjira::Feed.parse(xml)
       self.response = feed
-      return self
+      populate_attributes
+      save!
+      return_entries = build_articles
+
+      return Article.create(return_entries)
     rescue
+      # use most recent articles from feed
       puts "rescued"
       return nil
     end
   end
 
   def populate_attributes
-    if fetch_data
-      self.description = response.description ? response.description : ''
-      self.title = response.title
+    if response
+      find_alias(:title, response, "title")
+      find_alias(:description, response, "description")
+      find_alias(:image_url, response, "image_url")
       self
     else
       false
     end
   end
 
+  def ensure_id
+    id || save
+  end
+
   def build_articles
     return nil if !response.entries
+    return nil if !ensure_id
     return_entries = []
     response.entries.each do |entry|
       current = Article.new()
       current.populate_attributes(entry)
-      return_entries << current
+      current.feed_id = id
+
+      # single hit on databse
+      return_entries << current.attributes # get attr hash
+      return_entries[-1].delete("id") # remove extra keys
+      return_entries[-1].delete("created_at") # remove extra keys
+      return_entries[-1].delete("updated_at") # remove extra keys
     end
+    # create many articles with single hit on db
     return_entries
+  end
+
+  def thread_fetch
+    return nil if url.nil?
+    begin
+      xml = HTTParty.get(url).body
+      feed = Feedjira::Feed.parse(xml)
+      self.response = feed
+      # how to return from thread?
+      # what does .join look for?
+      return build_articles
+    rescue
+      # use most recent articles from feed
+      return nil
+    end
   end
 
   private
 
-  def find_alias(prop, item, *aliases)
+  def find_alias(prop, rss_item, *aliases)
     aliases.each do |ali|
-      if item.respond_to? ali
-        self.send((prop.to_s + "="), item.send(ali))
-        return item.send(ali)
+      if rss_item.respond_to?(ali) && !rss_item.send(ali).nil?
+        self.send((prop.to_s + "="), rss_item.send(ali))
+        return rss_item.send(ali)
       end
     end
+    self.send((prop.to_s + "="), "No #{prop} data available")
     false
   end
 
