c09c2d79db5628b7ad4cf35bd98d40253e7af573
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-12-01 09:56:49 -0800

    fix auth route

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { fetchAllFeeds } from "../actions/feed_actions";
 
-const Auth = ({component: Component, path, loggedIn, feeds}) => {
+const Auth = ({component: Component, path, loggedIn, feeds, fetchAllFeeds}) => {
+  if (Object.keys(feeds).length === 0) {
+    fetchAllFeeds();
+    return (
+      <div></div>
+    )
+  }
   const firstFeed = feeds[Object.keys(feeds)[0]];
   return (
     <Route path={path} render={(props) => (
@@ -25,6 +32,10 @@ const Protected = ({ component: Component, path, loggedIn }) => (
   )} />
 );
 
+const mapDispatchToProps = dispatch => ({
+  fetchAllFeeds: () => dispatch(fetchAllFeeds())
+});
+
 const mapStateToProps = state => (
   {
     loggedIn: Boolean(state.session.currentUser),
@@ -32,6 +43,6 @@ const mapStateToProps = state => (
   }
 );
 
-export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const AuthRoute = withRouter(connect(mapStateToProps, mapDispatchToProps)(Auth));
 
 export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
