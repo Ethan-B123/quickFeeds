28a5e25a56f703f36248ba4390779115e6d2a412
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2018-01-16 14:55:44 -0800

    add cloudinary to resize thumbnails

--- frontend/components/main/article_index_item.jsx
+++ frontend/components/main/article_index_item.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default ({key, article, openFn}) => {
+console.log(article.image === "No image data available");
+  const imageUrl = article.image === "No image data available"
+    ? ""
+    : "http://res.cloudinary.com/dhc8w148v/image/fetch/c_fill,g_center,h_80,q_100,w_140/" + article.image;
   return (
     <li onClick={openFn(article.id)} className="article" key={key}>
       <div
-        style={{backgroundImage: `url(${article.image})`}}
+        style={{backgroundImage: `url(${imageUrl})`}}
         className="article-image-container">
       </div>
       <div className="article-item-right-container">
@@ -14,3 +18,4 @@ export default ({key, article, openFn}) => {
     </li>
   );
 };
+// http://res.cloudinary.com/dhc8w148v/image/fetch/c_fill,g_center,h_80,q_100,w_140/http://www.nasa.gov/sites/default/files/thumbnails/image/nithin_mac_panel_0.jpg
--- frontend/components/main/article_show.jsx
+++ frontend/components/main/article_show.jsx
@@ -1,32 +1,91 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import ReadButton from "./read_button_container";
+let className = ""
 
-export default ({ article, key }) => {
-  const image = article.image === "No image data available" ?
-    false : article.image;
-  const imgSettings = image ?
-    { backgroundImage: `url(${article.image})` } :
-    { height: "0px" }
-  return (
-    <div className="article-show" key={key}>
-    <div className="article-show-top">
-    <ReadButton articleId={article.id} />
-    </div>
-      <div className="inner-section">
-        <div className="article-show-body">
-        <h1>{article.title}</h1>
-        <div>
-        {image ? <div className="show-image-container">
-          <img src={image} />
-        </div> : ""}
-
-        </div>
-        <p>{article.description}</p>
-        <sub>{article.publish_date.toLocaleString()}</sub>
-        </div>
-      </div>
-      <a className="nav-link link-button" target="_blank" href={article.url}>VISIT WEBSITE</a>
-    </div>
-  );
+function addClassTest() {
+  className = "show-image";
+  console.log("loaded");
 }
+
+class ArticleShow extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { styles: {
+      opacity: 0,
+      // maxHeight: "0%"
+    } }
+  }
+
+  imageLoaded() {
+    this.setState({
+      styles: {
+        opacity: 1,
+        maxWidth: "100%",
+        // maxHeight: "100%",
+        objectFit: "fill"
+      }
+    });
+  }
+
+  render() {
+    const { article, key } = this.props;
+    const image = article.image === "No image data available" ?
+      false : article.image;
+    const imgSettings = image ?
+      { backgroundImage: `url(${article.image})` } :
+      { height: "0px" }
+    return (
+      <div className="article-show" key={key}>
+      <div className="article-show-top">
+      <ReadButton articleId={article.id} />
+      </div>
+        <div className="inner-section">
+          <div className="article-show-body">
+          <h1>{article.title}</h1>
+          <div>
+          {image ? <div className="show-image-container">
+            <img onLoad={this.imageLoaded.bind(this)} style={this.state.styles} src={image} />
+          </div> : ""}
+
+          </div>
+          <p>{article.description}</p>
+          <sub>{article.publish_date.toLocaleString()}</sub>
+          </div>
+        </div>
+        <a className="nav-link link-button" target="_blank" href={article.url}>VISIT WEBSITE</a>
+      </div>
+    );
+  }
+}
+
+export default ArticleShow;
+
+// export default ({ article, key }) => {
+//   const image = article.image === "No image data available" ?
+//     false : article.image;
+//   const imgSettings = image ?
+//     { backgroundImage: `url(${article.image})` } :
+//     { height: "0px" }
+//   return (
+//     <div className="article-show" key={key}>
+//     <div className="article-show-top">
+//     <ReadButton articleId={article.id} />
+//     </div>
+//       <div className="inner-section">
+//         <div className="article-show-body">
+//         <h1>{article.title}</h1>
+//         <div>
+//         {image ? <div className="show-image-container">
+//           <img onLoad={addClassTest} className={className} src={image} />
+//         </div> : ""}
+//
+//         </div>
+//         <p>{article.description}</p>
+//         <sub>{article.publish_date.toLocaleString()}</sub>
+//         </div>
+//       </div>
+//       <a className="nav-link link-button" target="_blank" href={article.url}>VISIT WEBSITE</a>
+//     </div>
+//   );
+// }
